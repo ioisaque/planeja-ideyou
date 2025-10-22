0821c2cc1d9c20d42f15b056cea6b0b70c1881f9
Isaque Costa <costa@isaque.it>
2025-10-22 04:31:51 -0300

    name change

--- index.php
+++ index.php
@@ -30,7 +30,6 @@ if (true === false)
     <meta name="apple-touch-fullscreen" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="white">
-    <meta name="apple-mobile-web-app-title" content="Aurélio">
     <link rel="apple-touch-icon" href="assets/icons/apple-icon-180.png">
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0,viewport-fit=cover">
 

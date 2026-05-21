64baad38e62fb4f25c5dda31e0fae32577f605e5
isaque.it <costa@isaque.it>
2026-05-21 16:14:48 -0300

    log config added

--- init.php
+++ init.php
@@ -3,6 +3,14 @@
 define('COMPANY_NAME', 'IdeYou');
 define("BASEPATH",     str_replace("init.php", "", realpath(__FILE__)));
 
+$logDir = BASEPATH . 'logs';
+if (!is_dir($logDir))
+	mkdir($logDir, 0755, true);
+
+ini_set('log_errors', '1');
+ini_set('error_log', $logDir . '/php-error.log');
+error_reporting(E_ALL);
+
 require_once(BASEPATH . "lib/aux_functions.php");
 define('HOJE', DATA_F(false, 'Y-m-d', 'd/m/Y'));
 
